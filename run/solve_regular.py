29a01f429a2fea56c62da8d0ce07702882273a31
George Roscoe <roscoeg@thehutgroup.com>
2021-12-14 15:11:48 +0000

    My changes

--- run/solve_regular.py
+++ run/solve_regular.py
@@ -1,24 +1,85 @@
-import os
-import sys
-import pathlib
-import json
 import datetime
+import json
+import pathlib
+import sys
+from collections import Counter
+from tqdm import tqdm
+import pandas as pd
+base_folder = pathlib.Path()
+sys.path.append(str(base_folder / "../src"))
+from multi_period_dev import solve_multi_period_fpl
 
-if __name__=="__main__":
-    base_folder = pathlib.Path()
-    sys.path.append(str(base_folder / "../src"))
-    from multi_period_dev import connect, get_my_data, prep_data, solve_multi_period_fpl
 
-    with open('regular_settings.json') as f:
-        options = json.load(f)
+def run_solver(n):
+  responses = []
+  all_responses = []
+  next_gw_list = []
 
-    session, team_id = connect()
-    my_data = get_my_data(session, team_id)
-    data = prep_data(my_data, options)
-
+  for i in tqdm(range(n)):
+    print(f"ITERATION NUMBER {i+1}")
+    data, options = load_settings()
     result = solve_multi_period_fpl(data, options)
-    print(result['summary'])
-    time_now = datetime.datetime.now()
-    stamp = time_now.strftime("%Y-%m-%d_%H-%M-%S")
-    result['picks'].to_csv(f"results/regular_{stamp}.csv")
-    
+    responses.append(result)
+    print(result['summary'].split('GW')[1])
+
+
+  for (k, response) in enumerate(responses):
+    next_gw = response['summary'].split('GW')[1]
+    next_gw_list.append(next_gw)
+
+    all_responses.append(
+        pd.DataFrame([
+          (k,
+           response['summary'],
+           response['total_xp'])
+        ])
+    )
+  time_now = datetime.datetime.now()
+  stamp = time_now.strftime("%Y-%m-%d_%H-%M-%S")
+  counter = Counter(next_gw_list)
+  counter_results = open(f"results/counter_results_{stamp}.txt", "w")
+  cs = sorted(counter.items(), key=lambda n: n[1], reverse=True)
+  counter_results.write(", ".join(f"{el[1]} times: \n {el[0]}" for el in cs))
+
+  df = pd.concat(all_responses)
+  df.to_csv(f"results/multiobj_ws_solution_group_{stamp}.csv")
+
+
+def load_settings():
+  base_folder = pathlib.Path()
+  sys.path.append(str(base_folder / "../src"))
+  from multi_period_dev import connect, get_my_data, prep_data
+
+  with open('regular_settings.json') as f:
+    options = json.load(f)
+
+  session, team_id = connect()
+  my_data = get_my_data(session, team_id)
+  data = prep_data(my_data, options)
+
+  return data, options
+
+
+if __name__ == "__main__":
+  # base_folder = pathlib.Path()
+  # sys.path.append(str(base_folder / "../src"))
+  # from multi_period_dev import connect, get_my_data, prep_data, \
+  #   solve_multi_period_fpl
+  #
+  # with open('regular_settings.json') as f:
+  #   options = json.load(f)
+  #
+  # session, team_id = connect()
+  # my_data = get_my_data(session, team_id)
+  # data = prep_data(my_data, options)
+
+  run_solver(10)
+
+  # all_responses.to_csv(
+  #         f"results/multiobj_ws_solution_group_{stamp}.csv")
+
+  # result = solve_multi_period_fpl(data, options)
+  # print(result['summary'])
+  # time_now = datetime.datetime.now()
+  # stamp = time_now.strftime("%Y-%m-%d_%H-%M-%S")
+  # result['picks'].to_csv(f"results/regular_{stamp}.csv")

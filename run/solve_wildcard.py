a7dbeda506510228917e33f01b0f9d3398122f9a
George Roscoe <roscoeg@thehutgroup.com>
2021-12-23 09:50:36 +0000

    Adding in useful prints

--- run/solve_wildcard.py
+++ run/solve_wildcard.py
@@ -18,6 +18,22 @@ if __name__=="__main__":
 
     result = solve_multi_period_fpl(data, options)
     print(result['summary'])
+    current_gameweek = result['picks']['week'].iloc[0]
+    team_and_bench = result['picks'][['name', 'lineup', 'bench', 'captain','vicecaptain','week']]
+    current_week_team_and_bench = team_and_bench.loc[team_and_bench['week'] == current_gameweek]
+    captain = current_week_team_and_bench.loc[current_week_team_and_bench['captain'] == 1]['name'].values[0]
+    vicecaptain = current_week_team_and_bench.loc[current_week_team_and_bench['vicecaptain'] == 1]['name'].values[0]
+    lineup = current_week_team_and_bench.loc[current_week_team_and_bench['lineup'] == 1][['name', 'lineup']]
+    bench = current_week_team_and_bench.loc[current_week_team_and_bench['bench'] != -1][['name', 'bench']]
+    print("Captain:")
+    print(captain)
+    print("Vice Captain:")
+    print(vicecaptain)
+    print("")
+    print("Lineup")
+    print(lineup)
+    print("Bench")
+    print(bench)
     time_now = datetime.datetime.now()
     stamp = time_now.strftime("%Y-%m-%d_%H-%M-%S")
     result['picks'].to_csv(f"results/wildcard_{stamp}.csv")
